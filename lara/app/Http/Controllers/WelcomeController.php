b4da676a2b2bedac997f8f86cfd4dd4671e2732e
汤华茂 <764697898@qq.com>
2017-08-31 18:00:12 +0800

    add welcomeController

---
+++ lara/app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author tanghuamao@noahwm.com
+ * @datetime 2017-08-30 18:17
+ */
+namespace App\Http\Controllers;
+
+class WelcomeController {
+
+	public function index()
+	{
+		return 'welcome controller success';
+	}
+}
---
+++ lara/app/Http/routes.php
@@ -0,0 +1,6 @@
+<?php
+$app['router']->get('/', function () {
+	return 'hello world';
+});
+
+$app['router']->get('welcome', 'App\Http\Controllers\WelcomeController@index');
