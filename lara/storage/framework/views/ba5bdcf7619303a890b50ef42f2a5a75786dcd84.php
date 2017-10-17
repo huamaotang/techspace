ab97c2b4e8f6cf3b7025b0ba0bc0584748aaf789
汤华茂 <764697898@qq.com>
2017-10-17 11:19:44 +0800

    ci

---
+++ lara/storage/framework/views/ba5bdcf7619303a890b50ef42f2a5a75786dcd84.php
@@ -0,0 +1,8 @@
+
+
+<?php $__env->startSection('content'); ?>
+     <?php $__currentLoopData = $users; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $user): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <p><?php echo e($user->name); ?></p>
+     <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
